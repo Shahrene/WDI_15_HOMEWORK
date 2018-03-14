494298cf6987635564f69fe72d8f19ca324941d5
Shahrene Davey <shahrene.davey@gmail.com>
2018-03-14 20:46:37 +1100

    loops

---
+++ Shahrene/wk01/3-wed/loops.js
@@ -0,0 +1,21 @@
+
+for (var count = 0; count < 21; count++) {
+    if (count % 2 === 0) {
+      console.log(count + ' is Even');
+  	} else {
+      console.log(count + ' is Odd');
+  }
+}
+
+var num = 9;
+for (var count = 0; count < 11; count++) {
+  var result = count * num;
+  console.log(count + ' * ' + num + ' = ' + result);
+}
+
+var favCuisines = ['Mexican', 'Japanese', 'Italian', 'Indian']; {
+  console.log('My #1 choice is '+ favCuisines[0]);
+  console.log('My #2 choice is '+ favCuisines[1]);
+  console.log('My #3 choice is '+ favCuisines[2]);
+  console.log('My #4 choice is '+ favCuisines[3]);
+}
