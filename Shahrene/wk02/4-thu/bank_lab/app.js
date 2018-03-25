102da956e719748c8cd26f7f0cf42d121eeff4de
Shahrene Davey <shahrene.davey@gmail.com>
2018-03-25 21:14:28 +1100

    bank lab

--- Shahrene/wk02/4-thu/bank_lab/app.js
+++ Shahrene/wk02/4-thu/bank_lab/app.js
@@ -1,26 +1,60 @@
 
 //search for the deposit button by tag name
 //better choice may be giving it a class name in HTML and search by that
-var depositBtn = document.querySelector('.depositBtn');
-var withdrBtn = document.querySelector('.withdrBtn');
-var inputSave = document.querySelector('.inputSave');
-var saveResult = document.querySelector('.saveResult');
-
-var inputCheck = document.querySelector('.inputCheck');
-var checkResult = document.querySelector('.checkResult');
 //query selector returns an object with lots of properties
 //including properties to change the value and textContent
+//user enters amount into input field and clicks withdraw button
+//amount is subtracted from amount in result span
+//account can not be overdrawn and request is ignored if amount to be withdrawn is greater than balance
+//user enters amount into input field and clicks deposit button
+//amount goes into balance span -get the value from the input box
+//any further amount entered into field needs to be added if
+//deposit button is clicked and shown in span
 
-var deposit = function() {
-  var i = 1000;
-  var total = Number(inputSave.value) +i;
-  console.log('total');
-  saveResult.onclick = total;
-  // get the value from the input box - to do
+//Savings
+var depositBtn = document.querySelector('.depositBtn');
+var saveBal = document.querySelector('.saveBal');
+var inputSave = document.querySelector('.inputSave');
+var withdrBtn = document.querySelector('.withdrBtn');
 
-  //add value to balance and show in span - to do
-  //may be even just start by trying to set the span with the number 1000
-  //when user clicks on the deposit button
+saveBal.textContent = 0;
+
+var depositIntoSavings = function() {
+  var balance = Number(saveBal.textContent) + Number(inputSave.value);
+  saveBal.textContent = balance;
 }
-// tell button to run a function on click event
-depositBtn.addEventListener('click', deposit);
+depositBtn.addEventListener('click', depositIntoSavings);
+
+var withdrawSavings = function() {
+  var balance = ((Number(saveBal.textContent) - Number(inputSave.value)) >= 0)
+    saveBal.textContent = balance;
+    if (inputSave <= saveBal) {
+    } else {
+      console.log('Withdrawal amount exceeds balance');
+  }
+}
+withdrBtn.addEventListener('click', withdrawSavings);
+
+//Checking
+var inputCheck = document.querySelector('.inputCheck');
+var checkBal = document.querySelector('.checkBal');
+var checkDepositBtn = document.querySelector('.checkDepositBtn');
+var checkWithdrBtn = document.querySelector('.checkWithdrBtn');
+
+checkBal.textContent = 0;
+
+var depositIntoCheck = function() {
+  var checkingBalance = Number(checkBal.textContent) + Number(inputCheck.value);
+  checkBal.textContent = checkingBalance;
+}
+checkDepositBtn.addEventListener('click', depositIntoCheck);
+
+var withdrawChecking = function() {
+  var balance = Number(checkBal.textContent) - Number(inputCheck.value);
+    if (inputCheck <= checkBal) {
+      checkBal.textContent = balance;
+    } else {
+      console.log('Withdrawal amount exceeds balance');
+    }
+  }
+checkWithdrBtn.addEventListener('click', withdrawChecking);
