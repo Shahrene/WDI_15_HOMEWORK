a83a16fa054abe5b6d94b86fe321e024e9f685e8
Shahrene Davey <shahrene.davey@gmail.com>
2018-04-26 23:05:28 +1000

    Giphy Loco

--- Shahrene/wk07/2-tue/ajax_movies/app.js
+++ Shahrene/wk07/2-tue/ajax_movies/app.js
@@ -1,11 +1,59 @@
 
 
+var $queryInput = $('.query-input');
+var $resultsDiv = $('.results');
 var $searchBtn = $('.searchBtn');
-  $searchBtn.on('click', function (event){
-    event.preventDefault();
-    var $input = $('.input');
-    $.ajax ({
-        url: 'http://omdbapi.com/?s=' + $input + '&apikey=2f6435d9' }).done(function(res){
-    $('h2').text(res.Title);
-    });
+
+var showResults = function(res) {
+   res.Search.forEach(function(movie) {
+    var $h2 = $('<h2>').addClass('title')
+    var $a = $('<a>')
+      .attr('target', '_blank')
+      .attr('href', 'http://www.imdb.com/title/' + movie.imdbID)
+      .text(movie.Title);
+
+    $h2.append($a);
+
+    $resultsDiv.append($h2);
   });
+  }
+$searchBtn.on('click', function (event){
+  event.preventDefault();
+  $resultsDiv.empty();
+  var $searchForm = $('.search-form');
+  var options = {
+    url: 'http://omdbapi.com/?s=' + $queryInput.val() + '&apikey=2f6435d9'
+  }
+$.ajax(options).done(showResults)
+
+});
+
+
+
+
+//
+// var searchForm = document.querySelector('.search-form');
+// var queryInput = document.querySelector('.query-input');
+// var resultDiv = document.querySelector('.results');
+//
+// searchForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   console.log(queryInput.value);
+//
+//   var options = {
+//     url: 'http://omdbapi.com/?s=' + queryInput.value + '&apikey=2f6435d9'
+//   }
+//   // res if call back signature
+//   var showResults = function(res) {
+//     res.Search.forEach(function(movie) {
+//       //append it back to the dom
+//       var newElem = document.createElement('p')
+//       newElem.textContent = 'movie.Title'
+//       //
+//       resultDiv.appendChild(newElem);
+//     });
+//   }
+//
+//   $.ajax(options).done(showResults)
+//
+// })
---
+++ Shahrene/wk07/4-thu/giphy_loco/app.js
@@ -0,0 +1,29 @@
+
+
+var $queryInput = $('.query-input');
+var $resultsDiv = $('.results');
+var $searchBtn = $('.searchBtn');
+
+
+var showResults = function(res) {
+   res.data.forEach(function(gif) {
+    var $div = $('<div>');
+    var $gif = $('<img>');
+    attr('src', gif.images.fixed_height.url);
+    $div.append($gif);
+    $resultsDiv.append($div);
+  });
+  }
+
+$searchBtn.on('click', function (event){
+  event.preventDefault();
+  $resultsDiv.empty();
+  var $searchForm = $('.search-form');
+  var options = {
+    url: 'https://api.giphy.com/v1/gifs/search?=' + $queryInput.val() + '&api_key=SAAu8RpcPU9qVHa6Wnp83mhFRq16r2dB&q=&limit=10&offset=0&rating=G&lang=en'
+  }
+$.ajax(options).done(showResults)
+  console.log($queryInput.input);
+});
+
+// $(window).scroll(function() {
