3abea4b0ac7d43abcc77f90f36ca6813cc90d832
Shahrene Davey <shahrene.davey@gmail.com>
2018-05-05 17:56:35 +1000

    Giphy Loco complete

--- Shahrene/wk07/4-thu/giphy_loco/app.js
+++ Shahrene/wk07/4-thu/giphy_loco/app.js
@@ -3,34 +3,42 @@ var $searchForm = $('.search-form');
 var $queryInput = $('.query-input');
 var $resultsDiv = $('.results');
 var $searchBtn = $('.searchBtn');
-var offset = 0;
-var options = {
-   url: 'https://api.giphy.com/v1/gifs/search?q=' + $queryInput.val() + '&api_key=SAAu8RpcPU9qVHa6Wnp83mhFRq16r2dB&q=&limit=10&rating=G&lang=en&offset=' + offset
- }
+var number = 20;
+
+
 //Show gifs and append them to the div class 'results'
 var showResults = function(res) {
    res.data.forEach(function(gif) {
-
       var $div = $('<div>');
       var $gif = $('<img>');
-      $gif.attr('src', gif.images.fixed_height.url);
-      $div.append($gif);
-      $resultsDiv.append($div);
+        $gif.attr('src', gif.images.fixed_height.url);
+        $div.append($gif);
+        $resultsDiv.append($div);
+        number +=10;
   });
   }
-
 
 // search button function
 $searchBtn.on('click', function(event){
   event.preventDefault();
   $resultsDiv.empty();
   offset = 0;
-  //var Search = $queryInput.val()
   var options = {
-    url: 'http://api.giphy.com/v1/gifs/search?q=' + $queryInput.val() + '&api_key=SAAu8RpcPU9qVHa6Wnp83mhFRq16r2dB&q=&limit=10&rating=G&lang=en&offset=' + offset
+    url: 'http://api.giphy.com/v1/gifs/search?q=' + $queryInput.val() + '&api_key=SAAu8RpcPU9qVHa6Wnp83mhFRq16r2dB&limit=10&offset='+number
   }
-
 $.ajax(options).done(showResults)
+
 });
 
-// $(window).scroll(function() {
+$(window).on("scroll", function() {
+	var scrollHeight = $(document).height();
+	var scrollPosition = $(window).height() + $(window).scrollTop();
+	 if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+    offset = 0;
+	    // when scroll to bottom of the page
+      var options = {
+          url: 'http://api.giphy.com/v1/gifs/search?q=' + $queryInput.val() + '&api_key=SAAu8RpcPU9qVHa6Wnp83mhFRq16r2dB&limit=10&offset='+number
+         }
+       }
+       $.ajax(options).done(showResults)
+   });
