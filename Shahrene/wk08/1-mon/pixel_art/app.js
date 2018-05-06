57baaec0ffc3a7b9731af30d0fc470626a75f0a9
Shahrene Davey <shahrene.davey@gmail.com>
2018-05-06 19:47:52 +1000

    Commit 4

--- Shahrene/wk08/1-mon/pixel_art/app.js
+++ Shahrene/wk08/1-mon/pixel_art/app.js
@@ -1,29 +1,39 @@
 
 var $setColorBtn = $('.setColorBtn');
-var $colorDiv = $('.color-div');
+var $brushBox = $('.color-div');
 var $colorInput = $('.color-input');
 var $squares = $('.squares');
+var $div = $('.square')
+
+//create squares and append them to divs
+var createSquares = function() {
+    for (var i = 0; i < 650; i++) {
+      var $div = $('<div>')
+      $div.addClass('square')
+      $squares.append($div);
+    }
+  }
+
+createSquares();
+
+var paintBrush = function() {
+  var colorVal = $colorInput[0].value;
+    $brushBox[0].style.backgroundColor;
+      $brushBox.css('background-color',$colorInput.val());
+          $squares.on('mouseover', function(event){
+        if ($(event.target).hasClass('square')){
+          $(event.target).css('background-color',$colorInput.val());
+        }
+      })
+}
+paintBrush();
+
 
 $setColorBtn.on('click', function(event) {
   event.preventDefault();
-
   $colorInput.keypress(function(event) {
+    if (event.keycode === 13) {
     console.log( "Handler for .keypress() called." );
-  });
-
-  $colorInput = $('.color-input');
-  var colorVal = $colorInput[0].value;
-
-  $colorDiv[0].style.backgroundColor = colorVal;
+  }
 })
-
-//create squares and append them to divs
-$(document).ready(function() {
-    for (var i = 0; i < 675; i++) {
-      var $div = $('<div>')
-      $div.addClass('square')
-      //.mouseover(changeColor)
-      $squares.append($div);
-    }
-
-  })
+})
