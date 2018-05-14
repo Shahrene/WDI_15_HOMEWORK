e66442b2a82c8222e24a0fc2b06919789f50db89
Shahrene Davey <shahrene.davey@gmail.com>
2018-05-14 22:17:51 +1000

    sum of multiples

---
+++ Shahrene/wk10/1-mon/sum-of-multiples/sumOfMultiples.js
@@ -0,0 +1,14 @@
+
+
+var sumOfMultiples = function (num) {
+    var total = 0;
+    for (var i = 1; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+          console.log(i)
+            total = total + i;
+        }
+    }
+    console.log(total);
+}
+
+sumOfMultiples(10);
